90d22fbbd19da36cc0f371513350ea6dbd8f7922
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-06 19:48:44 -0700

    Created delete button

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -9,6 +9,38 @@ class App extends Component {
 			list:[]
 		}
 	}
+
+
+	updateInput(key, value){
+		this.setState({
+		 [key]: value
+		})
+	}
+   addItem() {
+   	const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+  };
+
+  const list = [...this.state.list];
+
+  list.push(newItem);
+
+  this.setState({
+  	list,
+  	newItem:""
+  });
+
+   	}
+
+   	deleteItem(id) {
+   		const list = [...this.state.list];
+
+   		const updatedList = list.filter(item => item.id != id);
+
+   		this.setState({list: updatedList});
+   	}
+   	
   render() {
     return (
       <div className="App">
@@ -27,6 +59,22 @@ class App extends Component {
         >
         Add
         </button>
+        <br/>
+        <ul>
+         {this.state.list.map(item => {
+         	return(
+         		<li key={item.id}>
+         	      {item.value}
+         	      <button
+         	      onClick ={() => this.deleteItem(item.id)}
+         	      >
+         	      X
+
+         	      </button>
+         	      </li>
+         	      )
+         })}
+        </ul>
         </div>
       </div>
     );
