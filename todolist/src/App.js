f05e66c47829e7c979b4eb5d6d619c1468320938
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-06 19:31:07 -0700

    Created Add button

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state={
+			newItem:"",
+			list:[]
+		}
+	}
   render() {
     return (
       <div className="App">
-        Hello
+        <div>
+        Add an item
+        
+        <br/>
+        <input
+        type="text"
+        placeholder="Type item here"
+        value={this.state.newItem}
+        onChange={e => this.updateInput("newItem", e.target.value)}
+        />
+        <button
+        onClick={() => this.addItem()}
+        >
+        Add
+        </button>
+        </div>
       </div>
     );
   }
